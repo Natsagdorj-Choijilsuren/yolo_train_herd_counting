6e6992547c715652a0b149507dd9e1dfc251ff28
Natsagdorj-Choijilsuren <choi@mm.inf.uec.ac.jp>
2025-10-11 14:51:27 +0900

    train yolo

--- src/train/train_yolo.py
+++ src/train/train_yolo.py
@@ -16,34 +16,77 @@ def parse_args()->argparse.Namespace:
 
     parser = argparse.ArgumentParser(
         description="Train a YOLO model with custom dataset and augmentations")
+    
     parser.add_argument(
         "--config", 
         type=str, 
         default="configs/train_config.yaml", 
         help="Path to the training configuration file"
     )
-    return parser.parse_args() 
 
-def train(config: dict):
+    parser.add_argument(
+        "--load_type", 
+        type=str, 
+        cchoices=["best", "last"],
+        default="best",
+        help="Type of weights to load: 'best' or 'last'"
+    )
+
+
+    args = parser.parse_args()
+    return args
+def train(config: dict,
+          args: argparse.Namespace)->None:
     # Load the YOLO model
     model = YOLO(config['training']['model'])  
-    
-    # Set up the dataset with augmentations
-    data_path = config['training']['data']
-    if data_path is None:
-        raise ValueError("Please provide a valid path to the custom dataset folder in the config file.")
-    
-    dataset = AlbumentationYOLODataset()
-    
-    # Train the model
+    pretrain_epoch = config['training']['epochs']*0.9
+
     model.train(
-        data=dataset,
-        epochs=config['training']['epochs'],
+        data=config['training']['data'],
+        epochs=pretrain_epoch,
         batch_size=config['training']['batch_size'],
         imgsz=config['training']['img_size'],
+        workers=config['training']['workers'],
         device=config['training']['device'],
-        name=config['training']['name']
+        name=config['training']['name'],
+        albu=albumentations_transform,
+        # use dataugmentation 
+        mosaic=config['augmentations'],
+        hsv_h= config['augmentations']['hsv_h'],
+        hsv_s= config['augmentations']['hsv_s'],
+        hsv_v= config['augmentations']['hsv_v'],
+        degrees= config['augmentations']['degrees'],
+        translate= config['augmentations']['translate'],
+        scale= config['augmentations']['scale'],
+        shear= config['augmentations']['shear'],
+        perspective= config['augmentations']['perspective'],
+        flipud= config['augmentations']['flipud'],
+        fliplr= config['augmentations']['fliplr'],
+        copy_paste= config['augmentations']['copy_paste'],
+        close_mosaic= config['augmentations']['close_mosaic']
+    )
+
+    # Fine-tune the model for the remaining epochs
+    model_path = f"runs/train/{config['training']['name']}/weights/{args.load_type}.pt"
+    model = YOLO(model_path)  # Load the best or last model
+    last_train_epoch = config['training']['epochs'] - pretrain_epoch
+
+    model.train(
+        data=config['training']['data'],
+        epochs=last_train_epoch,
+        batch_size=config['training']['batch_size'],
+        imgsz=config['training']['img_size'],
+        workers=config['training']['workers'],
+        device=config['training']['device'],
+        name=config['training']['name'],
+        albu=None, 
+        augment=False # Disable augmentations during fine-tuning
     )
 
 if __name__ == "__main__": 
-    pass 
+    args = parse_args()
+    config = load_config(args.config)
+    train(config, args)
+
+
+    
