411e774480905a356dff58ac5455a7ba83dc8aea
Natsagdorj-Choijilsuren <choi@mm.inf.uec.ac.jp>
2025-10-12 23:56:42 +0900

    count helpers

---
+++ src/utils/count_helpers.py
@@ -0,0 +1,15 @@
+import numpy as np
+from typing import Tuple, List, Optional, Dict 
+def get_sign(
+    object_center: Tuple[float, float],
+    line_start: Tuple[int, int],
+    line_end: Tuple[int, int]
+    ) -> int:
+    
+    """Determine the position of an object relative to a line."""
+    
+    d = (line_end[0] - line_start[0]) * (object_center[1] - line_start[1]) - \
+        (line_end[1] - line_start[1]) * (object_center[0] - line_start[0])
+
+    return np.sign(d) 
+
